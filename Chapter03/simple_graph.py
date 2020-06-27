19e5600281ddb3b84891ae19711f2eb622e2c410
Aaron Joseph <AaronJosephMathew@gmail.com>
2020-06-27 12:07:20 +0530

    Simple Graph

---
+++ Chapter03/simple_graph.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+A = tf.constant([[1,2], [3,4]], dtype=tf.float32)
+x = tf.constant([[0,10],[0,0.5]])
+b = tf.constant([[1,-1]],dtype=tf.float32)
+y = tf.add(tf.matmul(A,x),b, name="result")
+
+writer = tf.summary.create_file_writer("log/matmul")
+writer.close()
